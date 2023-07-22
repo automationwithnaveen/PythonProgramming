50d68517e7065d3e48efa86fea72229f2d084d66
siva krishna <sivauk786@gmail.com>
2023-07-22 19:08:35 +0530

    assignment

---
+++ demo.py
@@ -0,0 +1,43 @@
+print("Naveen Kumar")
+print('Sathya Technology')
+#print("Python')
+print("Naveen Kumar\nPython")
+print("Core\tAdvanced")
+print("S@athya\"Tech\"")
+#print("Sathya "Technology"")
+print("S@athya\"Tech\"")
+print("Hi\nStudents,This is Naveen,\nFrom Sathya Tech, AMPT")
+print("Hello Students this is 'Naveen'\nI am a \"python\" Faculty in \"Sathya\"\nMy Contact No is :'9052492329'")
+print("Sathya"+"Tech")
+print(5+2)
+print(12-6)
+print("12"+"5")
+print(10/2)
+print("Sathya","Tech")
+print("core","-","Python")
+print("\n","Sathya","-","Tech")
+#print("Python"+3)
+print("Python"+"3")
+
+#print(3+3+"Python")
+print("3"+"3"+"Python")
+
+#print("7"-2)
+print("7-2")
+
+#print("sathya"-"Tech")
+print("Sathya-Tech")
+
+print("Sathya\n\tTech")
+
+print("sathya\n\t\'Tech")
+
+print("**Sathya Tech**") 
+print("\n --- Python ---")
+
+print("\t\tnaveen\tKumar")
+print("\t\t=========")
+
+
+a=5
+print("a")
